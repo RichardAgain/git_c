68208247184ed290507f3700012b8ef45ffc960d
richardo <ricardohernande04@gmail.com>
2026-04-29 21:57:47 -0400

    refactor: improved hash-object function

--- src/commands/hash_object.c
+++ src/commands/hash_object.c
@@ -1,10 +1,9 @@
-
-#include "git/zpipe.h"
-#include <openssl/sha.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <zlib.h>
+
+#include "git/files.h"
+#include "git/objects.h"
 
 void hash_object(int argc, char *argv[]) {
   if (argc < 3) {
@@ -12,77 +11,34 @@ void hash_object(int argc, char *argv[]) {
     return;
   }
 
-  const char *param = argv[2];
-  const char *object_path = argv[3];
+  char *param = argv[2];
+  char *file_path = argv[3];
 
   if (strcmp(argv[2], "-w") != 0) {
     fprintf((stderr), "Argument not implemented");
     return;
   }
 
-  FILE *ofile = fopen(object_path, "rb");
+  file_result_t fr = read_file_from_path(file_path);
 
-  if (ofile == NULL) {
-    fprintf((stderr), "File not found");
+  unsigned char *contents = calloc(fr.size, sizeof(unsigned char));
+  if (contents == NULL)
+    return;
+
+  fread(contents, sizeof(unsigned char), fr.size, fr.file);
+
+  FILE *temp = tmpfile();
+  if (temp == NULL) {
+    perror("tmpfile");
     return;
   }
 
-  fseek(ofile, 0, SEEK_END);
-  long content_size = ftell(ofile);
-  rewind(ofile);
+  GitObject *object = write_git_object(contents, fr.size, temp);
 
-  char c;
-  int i = 0;
-  char contents[content_size];
-  while ((c = fgetc(ofile)) != EOF) {
-    contents[i] = c;
-    contents[i + 1] = '\0';
-    i++;
-  }
+  printf("%s\n", object->sha1_hex);
 
-  int header_size = snprintf(NULL, 0, "blob %ld", content_size);
+  fr = write_file_from_hex(object->sha1_hex, temp);
 
-  int file_size = header_size + 1 + content_size;
-
-  char to_hash[file_size];
-  snprintf(to_hash, header_size + 1, "blob %ld", content_size);
-  memcpy(to_hash + header_size + 1, contents, sizeof(contents));
-
-  unsigned char hash[SHA_DIGEST_LENGTH]; // == 20
-
-  SHA1((unsigned char *)to_hash, file_size, hash);
-
-  char sha1_hex[40];
-  for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
-    sprintf(sha1_hex + (i * 2), "%02x", hash[i]);
-  }
-  printf("%s\n", sha1_hex);
-
-  char file_path[56];
-  sprintf(file_path, ".git/objects/%c%c/%s", sha1_hex[0], sha1_hex[1],
-          sha1_hex + 2);
-
-  char folder_path[16];
-  snprintf(folder_path, sizeof(folder_path), "%s", file_path);
-
-  if (mkdir(folder_path, 0777) != 0) {
-    // printf("Didnt create folder");
-  };
-
-  FILE *temp_file = tmpfile();
-  FILE *result_file = fopen(file_path, "wb");
-
-  if (result_file == NULL || temp_file == NULL) {
-    perror("Error creating file(s)");
-    return;
-  }
-
-  fwrite(to_hash, sizeof(char), file_size, temp_file);
-  rewind(temp_file);
-
-  def(temp_file, result_file, Z_DEFAULT_COMPRESSION);
-
-  fclose(ofile);
-  fclose(temp_file);
-  fclose(result_file);
+  free(contents);
+  fclose(temp);
 }
